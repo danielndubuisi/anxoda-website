d58be9ec112f85f108556a1f6cb75158101dc49b
bright-droid <ndu24006@byui.edu>
2025-08-01 02:24:03 +0100

    build app

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,17 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/anxoda-website/", // Required for GitHub Pages
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+        Boolean
+    ),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
